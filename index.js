2e24a8a9bbb8268d7e146deb536cd6194a4f374f
Dice Yukita <5764240+diceky@users.noreply.github.com>
2022-08-29 21:58:40 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = process.env.PORT || 5000;
+
+let clients = [];
+
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+  clients.push(socket.id);
+
+  // Join a room
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // To all clients in room
+  socket.on("mood", (data) => {
+    io.in(roomId).emit("new_mood", {
+      clients: clients,
+      content: data,
+    });
+  });
+
+  socket.on("chat_message", (data) => {
+    io.in(roomId).emit("chat_message", data);
+  });
+
+  // To all clients in room except the sender
+  socket.on("handPos", (data) => {
+    io.to(roomId).emit("new_handPos", {
+      clients: clients,
+      content: data,
+    });
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    clients = clients.filter((item) => item !== socket.id);
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
